2aff1da5a4a8ff927c0688541aa38aa6df89c611
celly <ckavataki@gmail.com>
2025-12-26 14:49:39 +0300

    created requirements.txt

--- Eccommerce/MarketplaceApp/serializers.py
+++ Eccommerce/MarketplaceApp/serializers.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'email']
+        fields = ['id', 'first_name', 'last_name','username', 'password', 'email']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):

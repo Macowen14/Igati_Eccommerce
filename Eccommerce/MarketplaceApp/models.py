bd042b49b41c5f48a051b53437d26a1abd41d184
celly <ckavataki@gmail.com>
2025-12-30 13:29:59 +0300

    made changes at the  marketplace  app model by removing all models which have anything to do with sellers

--- Eccommerce/MarketplaceApp/models.py
+++ Eccommerce/MarketplaceApp/models.py
@@ -1,37 +1,6 @@
 from django.db import models
 from django.conf import settings
 
-
-class Seller(models.Model):
-    STATUS_CHOICES = (
-        ('pending', 'Pending'),
-        ('approved', 'Approved'),
-        ('rejected', 'Rejected'),
-    )
-
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='seller_profile')
-    business_name = models.CharField(max_length=255)
-    is_verified = models.BooleanField(default=False)
-    verified_at = models.DateTimeField(auto_now=True)
-    business_location = models.CharField(max_length=255)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
-    created_at = models.DateTimeField(auto_now_add=True)
-    rejection_reason = models.TextField(blank=True)
-
-    def __str__(self):
-        return f"{self.business_name} ({self.user.username})"
-
-class SellerDocument(models.Model):
-    seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name='documents')
-    document = models.FileField(upload_to='seller_docs/')
-    doc_type = models.CharField(max_length=100, help_text="e.g. ID, Business License")
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.doc_type} for {self.seller.business_name}"
-
-
-
 class category(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
